2ce200a88e6001adf23b9d5725ca96f669bb42f3
Sergio <sergiogonzalezyes@gmail.com>
2024-12-05 09:37:30 -0600

    day 1 part 2 complete

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,5 @@
-list1ex = [3, 4, 2, 1, 3, 3]
-list2ex = [4, 3, 5, 3, 9, 3]
+# list1ex = [3, 4, 2, 1, 3, 3]
+# list2ex = [4, 3, 5, 3, 9, 3]
 
 
 # list1 = []
@@ -42,29 +42,30 @@ list2ex = [4, 3, 5, 3, 9, 3]
 
 ## NOTE PART 2
 
-from collections import defaultdict
+list1 = []
+list2 = []
 
-sortedList1ex = sorted(list1ex)
-sortedList2ex = sorted(list2ex)
-hashmap = {}
+import csv
 
-for x, y in zip(sortedList1ex, sortedList2ex): 
-    if key in hashmap:
-        hashmap[key].append(value)
-    else:
-        hashmap[key] = [value]
+with open('day1/list_1.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+
+    for row in csv_reader:
+        list1.append(int(row[0]))
+
+with open('day1/list_2.csv', 'r') as file:
+    csv_reader = csv.reader(file)
+
+    for row in csv_reader:
+        list2.append(int(row[0]))
 
 
-print(sortedList1ex)
-print(sortedList2ex)
-print(hashmap)
+sortedList1 = sorted(list1)
+sortedList2 = sorted(list2)
+similarity_score = 0
 
+for x in sortedList1:
+    val = sortedList2.count(x) * x
+    similarity_score += val
 
-# sim_score = 0
-
-# for x, y in zip(sortedList1, sortedList2):
-
-#     val = abs(x - y)
-#     sim_score += val
-
-# print(difference)
+print(similarity_score)
