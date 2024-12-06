011fa919aefe620a74dcfd25404621cae1fabacd
Sergio <sergiogonzalezyes@gmail.com>
2024-12-05 21:14:24 -0600

    start day 2

---
+++ day2/day2.py
@@ -0,0 +1,17 @@
+#NOTE  DAY 2 PART 1
+
+with open('day2/inputday2example.txt', 'r') as file:
+        data = [list(map(int, line.split())) for line in file]
+
+# with open('day2/inputday2.txt', 'r') as file:
+#         data = [list(map(int, line.split())) for line in file]
+
+
+#NOTE LOGIC
+
+for x in data:
+        
+
+
+
+print(data)
