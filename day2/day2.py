dc31ab6929349d584396a21f5e55a3b06929ebaa
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2024-12-05 23:20:32 -0600

    reformatted part 1 bc of weird tabbing issue. I had started some of part 2 but will continue later

--- day2/day2.py
+++ day2/day2.py
@@ -1,34 +1,78 @@
 #NOTE  DAY 2 PART 1
 
 with open('day2/inputday2.txt', 'r') as file:
-        report = [list(map(int, line.split())) for line in file]
+    report = [list(map(int, line.split())) for line in file]
 
 #NOTE LOGIC
 safe_reports = 0
 safe = None
 
-
 for line in report:
-        sortedAsc = sorted(line)
-        sortedDesc = sorted(line, reverse=True)
-        if line != sortedAsc and line != sortedDesc:
-                continue
-        elif len(line) != len(set(line)):
-                continue
-        else: 
-                for index, level in enumerate(range(len(line) - 1), 1):
-                        val = line[index]
-                        difference = val - line[index - 1]
-                        
-                        if abs(difference) > 3 or abs(difference) < 1:
-                                safe = False
-                                break
-                        else:
-                                safe = True
-
-                if safe:
-                        safe_reports += 1
-                else:
-                        continue
+    sortedAsc = sorted(line)
+    sortedDesc = sorted(line, reverse=True)
+    if line != sortedAsc and line != sortedDesc:
+        continue
+    elif len(line) != len(set(line)):
+        continue
+    else: 
+        for index, level in enumerate(range(len(line) - 1), 1):
+            val = line[index]
+            difference = val - line[index - 1]        
+            if abs(difference) > 3 or abs(difference) < 1:
+                safe = False
+                break
+            else:
+                safe = True
+        if safe:
+            safe_reports += 1
+        else:
+            continue
 
 print('number of safe reports: ', safe_reports)
+
+
+
+
+
+
+
+# #NOTE  DAY 2 PART 2
+
+# # with open('day2/inputday2.txt', 'r') as file:
+# #         report = [list(map(int, line.split())) for line in file]
+
+# with open('day2/inputday2example.txt', 'r') as file:
+#     report = [list(map(int, line.split())) for line in file]
+
+# #NOTE LOGIC
+# safe_reports = 0
+# safe = None
+
+
+# for line in report:
+#     sortedAsc = sorted(line)
+#     sortedDesc = sorted(line, reverse=True)
+#     if line != sortedAsc and line != sortedDesc:
+#         continue
+#     elif len(line) != len(set(line)) and abs(len(set(line)) - len(line)) > 1:
+#         print(f'set {set(line)}')
+               
+#         print(f'line {line}')
+#         continue
+#     else: 
+#         for index, level in enumerate(range(len(line) - 1), 1):
+#             val = line[index]
+#             difference = val - line[index - 1]
+                       
+#             if abs(difference) > 3 or abs(difference) < 1:
+#                safe = False
+#                break
+#             else:
+#                safe = True
+
+#             if safe:
+#                 safe_reports += 1
+#             else:
+#                 continue
+
+# print('number of safe reports: ', safe_reports)
