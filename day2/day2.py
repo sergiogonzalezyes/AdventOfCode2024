05804c261081489523a0af69c1033ee701bf3cfa
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2024-12-05 22:47:21 -0600

    day 2 part 1 STAR GOT

--- day2/day2.py
+++ day2/day2.py
@@ -1,17 +1,34 @@
 #NOTE  DAY 2 PART 1
 
-with open('day2/inputday2example.txt', 'r') as file:
-        data = [list(map(int, line.split())) for line in file]
-
-# with open('day2/inputday2.txt', 'r') as file:
-#         data = [list(map(int, line.split())) for line in file]
-
+with open('day2/inputday2.txt', 'r') as file:
+        report = [list(map(int, line.split())) for line in file]
 
 #NOTE LOGIC
-
-for x in data:
-        
-
+safe_reports = 0
+safe = None
 
 
-print(data)
+for line in report:
+        sortedAsc = sorted(line)
+        sortedDesc = sorted(line, reverse=True)
+        if line != sortedAsc and line != sortedDesc:
+                continue
+        elif len(line) != len(set(line)):
+                continue
+        else: 
+                for index, level in enumerate(range(len(line) - 1), 1):
+                        val = line[index]
+                        difference = val - line[index - 1]
+                        
+                        if abs(difference) > 3 or abs(difference) < 1:
+                                safe = False
+                                break
+                        else:
+                                safe = True
+
+                if safe:
+                        safe_reports += 1
+                else:
+                        continue
+
+print('number of safe reports: ', safe_reports)
